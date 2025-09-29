22eab21ffefb04d3962f8a9889c94f3d12a06d12
john rochon <johnasrochon@gmail.com>
2025-09-29 13:55:03 -0500

    class1

---
+++ scripts/script.js
@@ -0,0 +1,49 @@
+console.log("Hello World from JS");
+
+// Double slashes are for comments on a single line
+/* This is a multi line comment
+*I can use it to describe a function or even a file
+*/
+
+//Variables and Constants
+// keyword nameVar = value;
+// var, let and const
+
+//Data Types
+//Strings (text), Numerical and Boolean
+// Strings are defined with " or ""
+var firstName = "John"; //string
+let lastName = "Rochon"; //string
+
+console.log(firstName);
+console.log(lastName);
+
+//Numbers
+let myAge = 42;
+let myHeight = 6.0;
+let myAgeTwo = "42";
+
+console.log(myAge);
+console.log(myHeight);
+console.log(myAgeTwo);
+
+//Boolean - true or false
+let isStudent = false;
+
+//Disply values in console
+//Concatenation
+console.log("My name is:" + firstName + " " + lastName + ", I'm " + myAge + "years old, my size is:" + myHeight);
+
+//Arithmetic Operations: + - / *
+let num1 = 10;
+let num2 = 2;
+
+let sum = num1 + num2;
+let sub = num1 - num2;
+let mul = num1 * num2;
+let div = num1 / num2;
+
+console.log("the sum is: " + sum);
+console.log("the sub is: " + sub);
+console.log("the mul is: " + mul);
+console.log("the div is: " + div);
