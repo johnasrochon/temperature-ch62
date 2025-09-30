3db8f20c28b41b2ff9e635e638bb5d92a7e1d491
john rochon <johnasrochon@gmail.com>
2025-09-30 09:23:42 -0500

    assignment1

---
+++ scripts/assignment1.js
@@ -0,0 +1,29 @@
+console.log("Want to Hear a Funny Story");
+
+//variables
+let character = "John";
+let action = "drink";
+let object = "tequila";
+let place = "5015";
+let time = "Sunday Funday";
+
+//using "const"
+const occasion = "party time";
+
+//story
+console.log("---See what had happend was---");
+console.log(character + " was at " + place + " for " + time + ".");
+console.log(" As soon as " + character + " saw " + object + " he decided it was a good idea to " + action + ".");
+console.log(" In his mind it was " + occasion + " turnt up!!!");
+
+//value change
+character = "Ashton";
+action = "eat";
+object = "cake";
+place = "davenport";
+time = "happy hour";
+
+//new story
+console.log(character + " was at " + place + " for " + time + ".");
+console.log(" As soon as " + character + " saw " + object + " he decided it was a good idea to " + action + ".");
+console.log(" In his mind it was " + occasion + " so he dove in face first!!!");
